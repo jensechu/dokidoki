d6764e58bf180fd6a6b1c655f225285cc9b0a925
Jensen <jensechu@gmail.com>
2015-07-05 20:24:46 -0700

    Mobile optimizations

--- mixlr.js
+++ mixlr.js
@@ -1,15 +1,15 @@
 "use strict";
 
 const getUserData = function(userID) {
-  const URL       = "https://api.mixlr.com/users/" + userID;
-  let mixlrAPI    = new XMLHttpRequest()
-  let container   = document.getElementById("status-container");
+  var URL         = "https://api.mixlr.com/users/" + userID;
+  var mixlrAPI    = new XMLHttpRequest()
+  var container   = document.getElementById("status-container");
 
   mixlrAPI.open("GET", URL, false);
   mixlrAPI.send();
 
-  let userData   = JSON.parse(mixlrAPI.responseText.trim());
-  let liveStatus = userData["is_live"]? "online" : "offline" ;
+  var userData   = JSON.parse(mixlrAPI.responseText.trim());
+  var liveStatus = userData["is_live"]? "online" : "offline" ;
 
   container.classList.add(liveStatus);
 }
